5c2e43c4fb45856c1f2ec03993fd9e605fbb5237
H.M.D.B. Bandara <dshnz29@gmail.com>
2026-05-18 23:54:55 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,9 @@ module.exports = {
   apps: [
     {
       name: "aiesec-link-bot",
-      script: "npm",
-      args: "start",
-      interpreter: "xvfb-run",
-      interpreter_args: "--auto-servernum --server-args='-screen 0 1280x1024x24'",
+      script: "src/index.js",
+      interpreter: "/usr/bin/xvfb-run",
+      interpreter_args: "--auto-servernum --server-args='-screen 0 1280x1024x24' /usr/bin/node",
       env: {
         NODE_ENV: "production",
       }
